e10d7b4c880bee4b6eab4be4970a96f3bda762e9
Elicia Reynolds <sk8ingchamp01@yahoo.com>
2023-12-02 22:54:28 -0500

    wrote Python code for New Client form submission and queried input to contacts table in database

--- app.py
+++ app.py
@@ -14,53 +14,57 @@ Session(app)
 # PLEASE LEAVE THIS COMMENTED UNTIL I AM FINISHED WITH EDITING THE TABLES!
 
 # Create Tables (Leave as commented once eschedule.db has been created after first run)
+conn = sqlite3.connect('eschedule.db')
+c = conn.cursor()
 
-# tables = [ """CREATE TABLE members (
-#        member_id INTEGER PRIMARY KEY AUTOINCREMENT,
-#        first_name TEXT NOT NULL,
-#        last_name TEXT NOT NULL,
-#        company_name TEXT NOT NULL,
-#        email TEXT NOT NULL,
-#        phone_number TEXT NOT NULL,
-#        username TEXT NOT NULL,
-#        password TEXT NOT NULL,
-#        hash TEXT NOT NULL
-#     );""",
-#     """CREATE TABLE contacts (
-#        contacts_id INTEGER PRIMARY KEY,
-#        first_name TEXT NOT NULL,
-#        last_name TEXT NOT NULL,
-#        phone_number TEXT NOT NULL,
-#        email TEXT NOT NULL,
-#        address TEXT NOT NULL,
-#        work_type TEXT NOT NULL,
-#        member_id INTEGER,
-#        FOREIGN KEY (member_id) REFERENCES members(member_id)
-#     );""",
+'''tables = [ """CREATE TABLE members (
+        member_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        first_name TEXT NOT NULL,
+        last_name TEXT NOT NULL,
+        company_name TEXT NOT NULL,
+        email TEXT NOT NULL,
+        phone_number TEXT NOT NULL,
+        username TEXT NOT NULL,
+        password TEXT NOT NULL,
+        hash TEXT NOT NULL
+     );""",
+     """CREATE TABLE contacts (
+        contacts_id INTEGER PRIMARY KEY,
+        first_name TEXT NOT NULL,
+        last_name TEXT NOT NULL,
+        phone_number TEXT NOT NULL,
+        email TEXT NOT NULL,
+        address TEXT NOT NULL,
+        work_type TEXT NOT NULL,
+        member_id INTEGER,
+        FOREIGN KEY (member_id) REFERENCES members(member_id)
+     );""",
 
-#     """CREATE TABLE schedule (
-#         month INTEGER NOT NULL,
-#         day INTEGER NOT NULL,
-#         year INTEGER NOT NULL,
-#         hour INTEGER CHECK (hour >= 1 AND hour < 24),
-#         minute INTEGER CHECK (minute >= 1 AND minute < 60),
-#         contacts_id INTEGER,
-#         FOREIGN KEY (contacts_id) REFERENCES contacts(contacts_id)
-#     );""",
+     """CREATE TABLE schedule (
+         month INTEGER NOT NULL,
+         day INTEGER NOT NULL,
+         year INTEGER NOT NULL,
+         hour INTEGER CHECK (hour >= 1 AND hour < 24),
+         minute INTEGER CHECK (minute >= 1 AND minute < 60),
+         contacts_id INTEGER,
+         FOREIGN KEY (contacts_id) REFERENCES contacts(contacts_id)
+     );""",
 
-#     """CREATE TABLE proposals (
-#         price INTEGER,
-#         description TEXT,
-#         contacts_id INTEGER,
-#         draft TEXT CHECK (draft IN ('draft','sent')),
-#         FOREIGN KEY (contacts_id) REFERENCES contacts(contacts_id)
-#     );"""
+     """CREATE TABLE proposals (
+         price INTEGER,
+         description TEXT,
+         contacts_id INTEGER,
+         draft TEXT CHECK (draft IN ('draft','sent')),
+         FOREIGN KEY (contacts_id) REFERENCES contacts(contacts_id)
+     );"""
 
-# ]
+ ]
 
-# for table in tables:
-#     c.execute(table)
+for table in tables:
+     c.execute(table)
 
+conn.commit()
+conn.close'''
 
 # Function to get the database connection
 def get_db():
@@ -106,9 +110,72 @@ def schedule():
 def clients():
      return render_template('clients.html')
 
-@app.route('/new_client')
+@app.route('/new_client', methods=["GET", "POST"])
+@login_required
 def new_client():
+    print("hello")
+    print(session["user_id"])
+     # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":        
+        first_name = request.form.get("first_name")
+        last_name = request.form.get("last_name")
+        phone_number = request.form.get("phone_number")
+        address = request.form.get("address")
+        work_type = request.form.get("work_type")
+        email = request.form.get("email")
+        member_id = session["user_id"]
+        email_pattern = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+
+        if not first_name or not last_name:
+            return apology("must provide first and last name", 400)
+        
+        elif not phone_number or len(phone_number) != 10 or not phone_number.isdigit():
+            return apology("must provide a valid phone number", 403)
+        
+        elif not email or not re.match(email_pattern,email):
+            return apology("must provide a valid email", 403)
+        
+        elif not address or not work_type:
+            return apology("must provide address and work type", 403)
+        
+        elif not member_id:
+            return apology ("must login before adding contacts")
+        
+         # Initialize the cursor outside the try block
+        c = None
+
+        try:
+            print("I'm BATMAN")
+            db = get_db()
+            c = db.cursor()
+
+            c.execute(
+                "INSERT INTO contacts (first_name, last_name, phone_number, email, address, work_type, member_id) VALUES(?,?,?,?,?,?,?)",
+                (first_name, last_name, phone_number, email, address, work_type, member_id))
+            
+            rows = c.execute(
+                "SELECT * FROM contacts WHERE member_id = ?", (session['user_id'],)
+                ).fetchall()
+            
+            print("I'm STILL BATMAN")
+            
+            db.commit()
+            print("BATMAN")
+            print(rows[0]["first_name"])
+            print(rows[0]["last_name"])
+
+            return redirect("/")
+        
+        except Exception as e:
+            print("NO!")
+            return f"Error: {str(e)}"
+
+        finally:
+            c.close()
+
+    else:
      return render_template('new_client.html')
+    
 @app.route("/logout")
 def logout():
     """Log user out"""
@@ -127,14 +194,12 @@ def login():
     if request.method == "POST":
         username = request.form.get("username")
         password = request.form.get("password")
-        print("I'm Batman!")
         # Initialize the cursor outside the try block
         c = None
         db = None
         try:
             db = get_db()
             c = db.cursor()
-            print("I'm Batman!")
             # Query database for username
             rows = c.execute(
                 "SELECT * FROM members WHERE username = ?", (username,)
@@ -235,7 +300,7 @@ def create_account():
             return f"Error: {str(e)}"
 
         finally:
-            return render_template ("home.html")
+            c.close()
 
     else:
         return render_template("create_account.html")
